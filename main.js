cd0fcaaa5dc498875b2aa92aac10094dacf5b263
Hugo <hugo.soares@outlook.com>
2020-10-14 21:03:07 -0300

    feat: change website looks to personal

---
+++ main.js
@@ -0,0 +1,12 @@
+function start () {
+  const backgroundCardElements = document.querySelectorAll('.background-card')
+
+  const backgroundCards = Array.from(backgroundCardElements)
+  backgroundCards.forEach((element, index) => {
+    setTimeout(() => element.classList.add('animate'), 1200 + index * 200)
+
+    setTimeout(() => element.classList.add('wiggle'), 3200)
+  })
+}
+
+window.addEventListener('DOMContentLoaded', start)
